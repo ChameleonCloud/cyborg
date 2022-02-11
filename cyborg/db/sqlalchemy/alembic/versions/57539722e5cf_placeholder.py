2b75d63757df65320437fed810cc7c847bacc2e6
Mark Powers <markpowers@uchicago.edu>
2022-02-11 15:01:35 -0600

    Fix DB migration dependency graph

--- cyborg/db/sqlalchemy/alembic/versions/57539722e5cf_placeholder.py
+++ cyborg/db/sqlalchemy/alembic/versions/57539722e5cf_placeholder.py
@@ -8,7 +8,7 @@ Create Date: 2019-12-03 00:55:16.012247
 
 # revision identifiers, used by Alembic.
 revision = '57539722e5cf'
-down_revision = 'c1b5abada09c'
+down_revision = '9f8da66547c3'
 
 from alembic import op
 import sqlalchemy as sa
